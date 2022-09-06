951e4e36bb09f17757071cec648c875e3e25ffd4
DaddybigRED <112836789+DaddybigRED@users.noreply.github.com>
2022-09-06 15:39:25 -0700

    Coded "GetStarted"
    99% That this is wrong

--- Assets/GetStarted.cs
+++ Assets/GetStarted.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GetStarted : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string GetStarted = "Question1";
 
-    // Update is called once per frame
-    void Update()
+    public void GetStartedButton()
     {
-        
+        SceneManager.LoadScene(GetStarted)
     }
 }
