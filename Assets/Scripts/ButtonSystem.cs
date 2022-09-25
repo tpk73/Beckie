52af676dc40a48f871572e8e1f1b38d65abb7128
DaddybigRED <112836789+DaddybigRED@users.noreply.github.com>
2022-09-24 20:00:48 -0700

    button system changes

--- Assets/Scripts/ButtonSystem.cs
+++ Assets/Scripts/ButtonSystem.cs
@@ -10,18 +10,54 @@ public class ButtonSystem : MonoBehaviour
     public GameObject m_latent;
     public GameObject m_patent;
     public GameObject m_plastic;
+    public GameObject m_porous;
+    public GameObject m_semiporous;
+    public GameObject m_nonporous; 
 
+    
 
     public void GetStarted()
     {
         m_mainMenu.SetActive(false);
         m_fingerPrint.SetActive(true);
     }
-
+    
     public void Latent()
     {
         m_fingerPrint.SetActive(false);
         m_latent.SetActive(true);
+    }
+    public void Porous()
+    {
+        m_latent.SetActive(false);
+        m_porous.SetActive(true);
+    
+    }
+    public void SemiPorous()
+    {
+        m_latent.SetActive(false);
+        m_semiporous.SetActive(true);
+
+    }
+    public void NonPorous()
+    {
+        m_latent.SetActive(false);
+        m_nonporous.SetActive(true);
+    
+    }
+    public void Back2fp()
+    {
+        m_latent.SetActive(false);
+        m_fingerPrint.SetActive(true);    
+    
+    }
+    public void Back2latent()
+    {
+        m_porous.SetActive(false);
+        m_semiporous.SetActive(false);
+        m_nonporous.SetActive(false);
+        m_latent.SetActive(true);
+    
     }
     public void Patent()
     {
@@ -33,7 +69,7 @@ public class ButtonSystem : MonoBehaviour
         m_fingerPrint.SetActive(false);
         m_plastic.SetActive(true);
     }
-
+    
     public void ReturnToMenu()
     {
         m_fingerPrint.SetActive(false);
