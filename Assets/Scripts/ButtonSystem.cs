093a668e62d3c5358bd2ff52023b68922d121905
DaddybigRED <112836789+DaddybigRED@users.noreply.github.com>
2022-11-03 15:57:18 -0700

    Dye types info

--- Assets/Scripts/ButtonSystem.cs
+++ Assets/Scripts/ButtonSystem.cs
@@ -23,6 +23,57 @@ public class ButtonSystem : MonoBehaviour
     public GameObject m_CAFumingBPAPlastic;
     public GameObject m_CAFumingWaxPaper;
     public GameObject m_CAFumingTile;
+    public GameObject m_DyeTypesM;
+    public GameObject m_R6GM;
+    public GameObject m_ardrox;
+    public GameObject m_MBDM;
+    public GameObject m_RAYM;
+    public GameObject m_RAMM;
+    public GameObject BasicY40M;
+    public GameObject m_NileRedM;
+    public GameObject m_PowderM;
+    public GameObject m_PhysicalDeveloperM;
+    public GameObject m_DyeTypesW;
+    public GameObject m_R6GW;
+    public GameObject m_ardroxW;
+    public GameObject m_MBDW;
+    public GameObject m_RAYW;
+    public GameObject m_RAMW;
+    public GameObject m_BasicY40W;
+    public GameObject m_NileRedW;
+    public GameObject m_PowderW;
+    public GameObject m_PhysicalDeveloperW;
+    public GameObject m_DyeTypesBPA;
+    public GameObject m_R6GBPA;
+    public GameObject m_ardroxBPA;
+    public GameObject m_MBDBPA;
+    public GameObject m_RAYBPA;
+    public GameObject m_RAMBPA;
+    public GameObject m_BasicY40BPA;
+    public GameObject m_NileRedBPA;
+    public GameObject m_PowderBPA;
+    public GameObject m_PhysicalDeveloperBPA;
+    public GameObject m_DyeTypesWP;
+    public GameObject m_R6GWP;
+    public GameObject m_ardroxWP;
+    public GameObject m_MBDWP;
+    public GameObject m_RAYWP;
+    public GameObject m_RAMWP;
+    public GameObject m_BasicY40WP;
+    public GameObject m_NileRedWP;
+    public GameObject m_PowderWP;
+    public GameObject m_PhysicalDeveloperWP;
+    public GameObject m_DyeTypesT;
+    public GameObject m_R6GT;
+    public GameObject m_ardroxT;
+    public GameObject m_MBDT;
+    public GameObject m_RAYT;
+    public GameObject m_RAMT;
+    public GameObject m_BasicY40T;
+    public GameObject m_NileRedT;
+    public GameObject m_PowderT;
+    public GameObject m_PhysicalDeveloperT;
+
 
     public void GetStarted()
     {
